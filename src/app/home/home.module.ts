1723bada196015941f07342e24f284391039f277
francescodist <francesco.dstf@gmail.com>
2018-09-15 18:01:18 +0200

    main template and styles completed

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomeComponent} from './home.component';
+import {UrlShortenerComponent} from '../url-shortener/url-shortener.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [HomeComponent]
+    imports: [
+        CommonModule,
+    ],
+    declarations: [HomeComponent, UrlShortenerComponent]
 })
-export class HomeModule { }
+export class HomeModule {
+}
---
+++ src/app/url-shortener/url-shortener.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UrlShortenerComponent } from './url-shortener.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [UrlShortenerComponent]
+})
+export class UrlShortenerModule { }
